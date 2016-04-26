fd9375326c8a8dd462480956bd7935df5470e13f
Jérémie Boulay <jeremi.boulay@gmail.com>
2016-04-26 12:41:05 +0200

    :arrow_up: :gem: add OF dependencies

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -2,70 +2,105 @@
 
 //--------------------------------------------------------------
 void ofApp::setup(){
-
+    cout << "setup" << endl;
+    
+    ofSetFrameRate(60);
+    ofSetLogLevel(OF_LOG_VERBOSE);
+    ofSetCircleResolution(60);
+    
+    server_receive.setup(RECEIVER_PORT);
+    server = *new ServerController();
+    
+    // Init the kinect after OSC servers
+    kinect.init();
 }
 
 //--------------------------------------------------------------
 void ofApp::update(){
-
+    
+    //NODE BRIDGE CHECK MESSAGE RECEIVE
+    while(server_receive.hasWaitingMessages()){
+        ofxOscMessage m;
+        server_receive.getNextMessage(&m);
+        string address = m.getAddress();
+        cout << address << endl;
+        
+        server.checkAddress(address);
+    }
+    
+    // Kinect update
+    if(server.kinectIsConnected() != kinect.kinectIsConnected()){
+        server.setKinectStatus(kinect.kinectIsConnected());
+        server.sendKinectStatusChange(server.kinectIsConnected());
+    }
+    
+    kinect.update();
 }
 
 //--------------------------------------------------------------
 void ofApp::draw(){
+    ofBackground(100, 100, 100);
+    ofSetColor(255, 255, 255);
+    kinect.draw();
+    
+    // COMMUNICATION INFORMATION
+    stringstream reportStream;
+    reportStream << "Node.js Server: " << ((server.isStarted())?"ON":"OFF") << endl
+    << "Web Render: " << ((server.webRenderIsConnected())?"ON":"OFF") << endl
+    << "Kinect: " << ((server.kinectIsConnected())?"ON":"OFF - press (o) :: try to connect the kinect.") << endl;
+    ofDrawBitmapString(reportStream.str(), 10, 20);
+}
 
+void ofApp::exit(){
+    server.sendOFStatusChange(false);
 }
 
 //--------------------------------------------------------------
 void ofApp::keyPressed(int key){
-
+    kinect.onKeyPressed(key);
 }
 
 //--------------------------------------------------------------
 void ofApp::keyReleased(int key){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::mouseMoved(int x, int y ){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::mouseDragged(int x, int y, int button){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::mousePressed(int x, int y, int button){
-
+    server.sendPlayCube(-1, -1, x, y);
 }
 
 //--------------------------------------------------------------
 void ofApp::mouseReleased(int x, int y, int button){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::mouseEntered(int x, int y){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::mouseExited(int x, int y){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::windowResized(int w, int h){
-
+    
 }
 
 //--------------------------------------------------------------
-void ofApp::gotMessage(ofMessage msg){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::dragEvent(ofDragInfo dragInfo){ 
-
+void ofApp::dragEvent(ofDragInfo dragInfo){
+    
 }
--- src/ofApp.h
+++ src/ofApp.h
@@ -1,24 +1,36 @@
 #pragma once
 
 #include "ofMain.h"
+#include "ofxOsc.h"
+#include "ServerController.h"
+#include "KinectController.h"
+
+
+#define RECEIVER_PORT 5555
 
 class ofApp : public ofBaseApp{
-
-	public:
-		void setup();
-		void update();
-		void draw();
-
-		void keyPressed(int key);
-		void keyReleased(int key);
-		void mouseMoved(int x, int y );
-		void mouseDragged(int x, int y, int button);
-		void mousePressed(int x, int y, int button);
-		void mouseReleased(int x, int y, int button);
-		void mouseEntered(int x, int y);
-		void mouseExited(int x, int y);
-		void windowResized(int w, int h);
-		void dragEvent(ofDragInfo dragInfo);
-		void gotMessage(ofMessage msg);
-		
+    
+public:
+    void setup();
+    void update();
+    void draw();
+    void exit();
+    
+    void keyPressed(int key);
+    void keyReleased(int key);
+    void mouseMoved(int x, int y );
+    void mouseDragged(int x, int y, int button);
+    void mousePressed(int x, int y, int button);
+    void mouseReleased(int x, int y, int button);
+    void mouseEntered(int x, int y);
+    void mouseExited(int x, int y);
+    void windowResized(int w, int h);
+    void dragEvent(ofDragInfo dragInfo);
+    
+private:
+    ServerController server;
+    // !!!! THE RECEIVER DOESN'T WORK INTO NODEBRIGDE
+    ofxOscReceiver server_receive;
+    
+    KinectController kinect;
 };
